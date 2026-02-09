b85ea5509dbfce52c7c93818baaa635ccb4537a0
Kwesil <nwabikejr@gmail.com>
2026-02-09 12:50:29 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+"""
+Docstring for backend_projects.task_tracker_cli.app
+-> Plan
+--> Step 1: Create a simple task tracker that can add, list, and mark tasks as complete. Store the tasks in a JSON file.
+
+
+"""
+
+import json
+import os
+import sys
+from datetime import datetime
